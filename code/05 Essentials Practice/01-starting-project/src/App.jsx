ca5f7048c8397bba1bdf789ea2b8b67c7d172f06
Andrzej Ołdak <aoldak@onshift.com>
2024-06-07 16:29:53 +0200

    Finish starting project

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,11 +1,30 @@
 import logo from './assets/investment-calculator-logo.png'
 import Header from "./compoonents/Header.jsx";
 import UserInput from "./compoonents/UserInput.jsx";
+import {useState} from "react";
+import Results from "./compoonents/Results.jsx";
+import {calculateInvestmentResults} from "./util/investment.js"
 
 function App() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    })
+        
+    function handleChange(inputIdentifier, newValue){
+        setUserInput((prevInput)=>{
+            return {
+                ...prevInput,
+                [inputIdentifier]: +newValue
+            }
+        })
+    }
     return <>
         <Header/>
-        <UserInput/>
+        <UserInput userInput={userInput} onChange={handleChange}/>
+        <Results resultsData={calculateInvestmentResults(userInput)}/>
     </>
 }
 
--- code/05 Essentials Practice/01-starting-project/src/compoonents/UserInput.jsx
+++ code/05 Essentials Practice/01-starting-project/src/compoonents/UserInput.jsx
@@ -1,24 +1,42 @@
-export default function () {
+export default function ({userInput, onChange}) {
     return <section id="user-input">
         <div className="input-group">
             <p>
                 <label htmlFor="initial_investment">INITIAL INVESTMENT:</label>
-                <input type="number" id="initial_investment" name="initial_investment"/>
+                <input type="number" id="initial_investment" name="initial_investment"
+                       value={userInput.initialInvestment}
+                       onChange={(e) => {
+                           onChange('initialInvestment', e.target.value)
+                       }}
+                />
             </p>
             <p>
                 <label htmlFor="annual_investment">ANNUAL INVESTMENT:</label>
-                <input type="number" id="annual_investment" name="annual_investment"/>
+                <input type="number" id="annual_investment" name="annual_investment"
+                       value={userInput.annualInvestment}
+                       onChange={(e) => {
+                           onChange('annualInvestment', e.target.value)
+                       }}
+                />
             </p>
         </div>
 
         <div className="input-group">
             <p>
                 <label htmlFor="expected_return">EXPECTED RETURN:</label>
-                <input type="number" id="expected_return" name="expected_return"/>
+                <input type="number" id="expected_return" name="expected_return" value={userInput.expectedReturn}
+                       onChange={(e) => {
+                           onChange('expectedReturn', e.target.value)
+                       }}
+                />
             </p>
             <p>
                 <label htmlFor="duration">DURATION:</label>
-                <input type="number" id="duration" name="duration"/>
+                <input type="number" id="duration" name="duration" value={userInput.duration}
+                       onChange={(e) => {
+                           onChange('duration', e.target.value)
+                       }}
+                />
             </p>
         </div>
 
