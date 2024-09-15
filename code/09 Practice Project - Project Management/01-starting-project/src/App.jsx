6a9555689104c1fcc7870cdedcee4ebb80f576cd
Andrzej Ołdak <aoldak@onshift.com>
2024-09-15 14:49:30 +0200

    Delete project

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -3,34 +3,56 @@ import {useRef, useState} from "react";
 function App() {
     const [projects, setProjects] = useState([]);
     const [addingProject, setAddingProject] = useState(false);
+    const [showProject, setShowProject] = useState(-1);
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
-    
-    function handleAddProject() { 
-        setProjects([...projects, {title: title.current.value, description: description.current.value, dueDate: dueDate.current.value}]);
+
+    function handleAddProject() {
+        setProjects([...projects, {
+            title: title.current.value,
+            description: description.current.value,
+            dueDate: dueDate.current.value
+        }]);
         setAddingProject(false);
     }
+    
+    function handleRemoveProject() {
+        setProjects(projects.filter((val, idx) => idx !== showProject))
+        setShowProject(-1);
+    }
+
     return (
         <>
             <div className="container">
                 <div className="menu">
                     <h2>Your projects:</h2>
-                    {projects.map(project => <div key={project.title}>{project.title}</div>)}
-                    <button className="btn btn-primary" onClick={()=>setAddingProject(true)} disabled={addingProject}>Add Project</button>
+                    {projects.map((project, index) => <div key={index}
+                                                           onClick={() => setShowProject(index)}>{project.title}</div>)}
+                    <button className="btn btn-primary" onClick={() => setAddingProject(true)}
+                            disabled={addingProject}>Add Project
+                    </button>
                 </div>
                 <div className="content">
                     {addingProject && <>
-                    <button className="btn" onClick={()=>setAddingProject(false)} >Cancel</button>
-                    <button className="btn btn-primary" onClick={handleAddProject} >Save</button>
-                    <input ref={title} type={"text"}/>
-                    <input ref={description} type={"text"}/>
-                    <input ref={dueDate} type={"date"}/>
+                        <button className="btn" onClick={() => setAddingProject(false)}>Cancel</button>
+                        <button className="btn btn-primary" onClick={handleAddProject}>Save</button>
+                        <input ref={title} type={"text"}/>
+                        <input ref={description} type={"text"}/>
+                        <input ref={dueDate} type={"date"}/>
                     </>}
-                    {!addingProject && <>
-                    <h1>No Project Selected</h1>
-                    <p>Select a project or get started with a new one</p>
-                    <button className="btn btn-primary" onClick={()=>setAddingProject(true)} disabled={addingProject}>Add Project</button>
+                    {!addingProject && showProject < 0 && <>
+                        <h1>No Project Selected</h1>
+                        <p>Select a project or get started with a new one</p>
+                        <button className="btn btn-primary" onClick={() => setAddingProject(true)}
+                                disabled={addingProject}>Add Project
+                        </button>
+                    </>}
+                    {!addingProject && showProject > -1 && <>
+                        <h1>{projects[showProject].title}</h1>
+                        <div>{projects[showProject].description}</div>
+                        <div>{projects[showProject].dueDate}</div>
+                        <button onClick={handleRemoveProject}>Delete Project</button>
                     </>}
                 </div>
             </div>
