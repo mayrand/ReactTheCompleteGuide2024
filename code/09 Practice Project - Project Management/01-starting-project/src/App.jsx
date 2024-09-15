408fed24d7873c8e095587a51fd4633d43fc88c6
Andrzej Ołdak <aoldak@onshift.com>
2024-09-15 13:49:09 +0200

    Project management adding project

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -1,9 +1,41 @@
+import {useRef, useState} from "react";
+
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+    const [projects, setProjects] = useState([]);
+    const [addingProject, setAddingProject] = useState(false);
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    
+    function handleAddProject() { 
+        setProjects([...projects, {title: title.current.value, description: description.current.value, dueDate: dueDate.current.value}]);
+        setAddingProject(false);
+    }
+    return (
+        <>
+            <div className="container">
+                <div className="menu">
+                    <h2>Your projects:</h2>
+                    {projects.map(project => <div key={project.title}>{project.title}</div>)}
+                    <button className="btn btn-primary" onClick={()=>setAddingProject(true)} disabled={addingProject}>Add Project</button>
+                </div>
+                <div className="content">
+                    {addingProject && <>
+                    <button className="btn" onClick={()=>setAddingProject(false)} >Cancel</button>
+                    <button className="btn btn-primary" onClick={handleAddProject} >Save</button>
+                    <input ref={title} type={"text"}/>
+                    <input ref={description} type={"text"}/>
+                    <input ref={dueDate} type={"date"}/>
+                    </>}
+                    {!addingProject && <>
+                    <h1>No Project Selected</h1>
+                    <p>Select a project or get started with a new one</p>
+                    <button className="btn btn-primary" onClick={()=>setAddingProject(true)} disabled={addingProject}>Add Project</button>
+                    </>}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
